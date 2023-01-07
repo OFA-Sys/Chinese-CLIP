548df31b2680049c409ea8cd59363f839b237fad
Yang An <yangapku@gmail.com>
2023-01-07 23:15:44 +0800

    Update model.py

--- cn_clip/clip/model.py
+++ cn_clip/clip/model.py
@@ -378,8 +378,8 @@ class CLIP(nn.Module):
     def encode_image(self, image, mask_ratio=0):
         if isinstance(self.visual, ModifiedResNet):
             if mask_ratio > 0:
-                raise NotImplementedError("Error: mask_ratio > 0 is currently only implemented for VisualTransformer." + \
-                    "If using ResNet, please set --mask-ratio to 0.0")
+                logging.info("Note: mask_ratio > 0 (FLIP strategy) is currently only implemented for VisualTransformer. " + \
+                    "It will not function for ResNet backbone.")
             return self.visual(image.type(self.dtype))
         return self.visual(image.type(self.dtype), mask_ratio)
 
