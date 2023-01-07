8ccc7932bd6ca3e7673c983fac5ead9581cb5441
Yang An <yangapku@gmail.com>
2023-01-07 23:26:42 +0800

    Update main.py
    Add note for mask_ratio

--- cn_clip/training/main.py
+++ cn_clip/training/main.py
@@ -181,6 +181,11 @@ def main():
             logging.info(f"  {name}: {val}")
     logging.info(f"Use GPU: {args.local_device_rank} for training")
 
+    # Note for mask_ratio
+    if args.mask_ratio > 0 and args.vision_model in ['RN50']:
+	logging.info("Note: mask_ratio > 0 (FLIP strategy) is currently only implemented for VisualTransformer. " + \
+	    "It will not function for ResNet backbone.")    
+
     # Optionally resume from a checkpoint
     start_epoch = 0
     steps = 0
